e299f269c0312cb25d1d00c8c76083c8f484770a
macs97 <marcela.alexandra97@gmail.com>
2020-08-24 02:21:25 -0500

    Finish master detail for Campuses, Sections and Churches

--- OnChurch.Web/Controllers/CampusesController.cs
+++ OnChurch.Web/Controllers/CampusesController.cs
@@ -144,6 +144,8 @@ namespace OnChurch.Web.Controllers
             }
 
             Campus campus = await _context.Campuses
+                .Include(s => s.Sections)
+                .ThenInclude(c => c.Churches)
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (campus == null)
             {
@@ -214,7 +216,227 @@ namespace OnChurch.Web.Controllers
 
             return View(section);
         }
+        public async Task<IActionResult> EditSection(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
+            Section section = await _context.Sections.FindAsync(id);
+            if (section == null)
+            {
+                return NotFound();
+            }
+
+            Campus campus = await _context.Campuses.FirstOrDefaultAsync(c => c.Sections.FirstOrDefault(d => d.Id == section.Id) != null);
+            section.IdCampus = campus.Id;
+            return View(section);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditSection(Section section)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(section);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction($"{nameof(Details)}/{section.IdCampus}");
+
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "There are a record with the same name.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+            return View(section);
+        }
+
+        public async Task<IActionResult> DeleteSection(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Section section = await _context.Sections
+                .Include(d => d.Churches)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (section == null)
+            {
+                return NotFound();
+            }
+
+            Campus campus = await _context.Campuses.FirstOrDefaultAsync(c => c.Sections.FirstOrDefault(d => d.Id == section.Id) != null);
+            _context.Sections.Remove(section);
+            await _context.SaveChangesAsync();
+            return RedirectToAction($"{nameof(Details)}/{campus.Id}");
+        }
+
+        public async Task<IActionResult> DetailsSection(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Section section = await _context.Sections
+                .Include(s => s.Churches)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (section == null)
+            {
+                return NotFound();
+            }
+
+            Campus campus = await _context.Campuses.FirstOrDefaultAsync(c => c.Sections.FirstOrDefault(d => d.Id == section.Id) != null);
+            section.IdCampus = campus.Id;
+            return View(section);
+        }
+
+        public async Task<IActionResult> AddChurch(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Section section = await _context.Sections.FindAsync(id);
+            if (section == null)
+            {
+                return NotFound();
+            }
+
+            Church model = new Church { IdSection = section.Id };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddChurch(Church church)
+        {
+            if (ModelState.IsValid)
+            {
+                Section section = await _context.Sections
+                    .Include(d => d.Churches)
+                    .FirstOrDefaultAsync(c => c.Id == church.IdSection);
+                if (section == null)
+                {
+                    return NotFound();
+                }
+
+                try
+                {
+                    church.Id = 0;
+                    section.Churches.Add(church);
+                    _context.Update(section);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction($"{nameof(DetailsSection)}/{section.Id}");
+
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "There are a record with the same name.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+
+            return View(church);
+        }
+
+        public async Task<IActionResult> EditChurch(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Church church = await _context.Churches.FindAsync(id);
+            if (church == null)
+            {
+                return NotFound();
+            }
+
+            Section section = await _context.Sections.FirstOrDefaultAsync(d => d.Churches.FirstOrDefault(c => c.Id == church.Id) != null);
+            church.IdSection = section.Id;
+            return View(church);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditChurch(Church church)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(church);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction($"{nameof(DetailsSection)}/{church.IdSection}");
+
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "There are a record with the same name.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+            return View(church);
+        }
+
+        public async Task<IActionResult> DeleteChurch(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Church church = await _context.Churches
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (church == null)
+            {
+                return NotFound();
+            }
+
+            Section section = await _context.Sections.FirstOrDefaultAsync(d => d.Churches.FirstOrDefault(c => c.Id == church.Id) != null);
+            _context.Churches.Remove(church);
+            await _context.SaveChangesAsync();
+            return RedirectToAction($"{nameof(DetailsSection)}/{section.Id}");
+        }
 
     }
+
 }
